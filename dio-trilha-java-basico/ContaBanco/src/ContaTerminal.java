0fe1b6df301efe98f5b6c59ccece92fbd857c84b
gxrj <gsouzafone@gmail.com>
2023-08-19 02:40:46 -0300

    feat: solução do desafio 'Simulando Uma Conta Bancária Através Do Terminal/Console'

---
+++ dio-trilha-java-basico/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main( String... args ) throws Exception {
+        Scanner input = new Scanner( System.in );
+
+        String agencia = obterAgencia( input );
+        int numero = obterNumeroConta( input );
+        String nomeCliente = obterNomeCliente( input );
+        float saldo = obterSaldo( input );
+
+        input.close();
+
+        String msg = "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d. Seu saldo é de %.2f reais";
+        msg += ( saldo > 0  ) ? " e já está disponível para saque. %n" : ". %n";
+
+        System.out.printf( msg, nomeCliente, agencia, numero, saldo );
+    }
+
+    private static String obterAgencia( Scanner input ) {
+        System.out.println( "Por favor, digite o número da Agência !" );
+        String agencia = "";
+        
+        do {
+            System.out.print( "Agência (apenas números e hífen caso possua dígito): " );
+            agencia = input.nextLine();
+        } while( !agencia.matches( "[0-9-]*" ) );
+
+        return agencia;
+    }
+    
+    private static int obterNumeroConta( Scanner input ) {
+        System.out.println( "Por favor, digite o número da Conta !" );
+        int numero = 0;
+        boolean excecaoDetectada;
+
+        do{
+            try{
+                System.out.print( "Número da conta (apenas números): " );
+                numero = Integer.parseInt( input.nextLine() );
+                excecaoDetectada = false;
+            }
+            catch( Exception e ) {
+                excecaoDetectada = true;
+            }
+        } while( excecaoDetectada );
+
+        numero = ( numero < 0 ) ? -numero : numero;  
+        
+        return numero;
+    }
+
+    private static String obterNomeCliente( Scanner input ) {
+        System.out.println( "Por favor, informe o Nome do Titular !" );
+        String nomeCliente = "";
+
+        do {
+            System.out.print( "Nome do titular(apenas letras são permitidas): " );
+            nomeCliente = input.nextLine();
+        } while( !nomeCliente.matches( "[A-Za-z\\s]*" ) );
+
+        return nomeCliente;
+    }
+
+    private static float obterSaldo( Scanner input ) {
+        
+        System.out.println( "Por favor, informe o valor do Saldo !" );
+        float saldo = 0;
+        boolean excecaoDetectada;
+
+        do {
+            try{
+                System.out.print( "Saldo (use vírgulas para valores decimais): " );
+                saldo = Float.parseFloat( input.nextLine().replace( ",", "." ) ); 
+                excecaoDetectada = false;
+            }
+            catch( Exception e ) {
+                excecaoDetectada = true;
+            }
+        } while( excecaoDetectada );
+
+        return saldo;
+    }
+
+}
