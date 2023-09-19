92e1d7191f7c527e4b7b07e5501c67996215a86d
gxrj <gsouzafone@gmail.com>
2023-09-18 22:03:20 -0300

    feat: acrescenta a resolucao do desafio do carrinho de compras

--- dio-trilha-java-basico/DesafioCollections/README.md
+++ dio-trilha-java-basico/DesafioCollections/README.md
@@ -1,3 +1,5 @@
 ## Exercícios e desafios da trilha "Conhecendo Collections Java"
 
 [Minha implementação](src/ListaTarefas.java) do exercício proposto pelo [enunciado de listas](https://github.com/cami-la/collections-java-api-2023/tree/master/out/production/collections-java-api-2023/main/java/list#1-lista-de-tarefas)
+
+[Minha implementação](src/CarrinhoDeCompras.java) do desafio proposto pelo [enunciado de listas](https://github.com/cami-la/collections-java-api-2023/tree/master/out/production/collections-java-api-2023/main/java/list#2-carrinho-de-compras)
--- dio-trilha-java-basico/DesafioCollections/src/CarrinhoDeCompras.java
+++ dio-trilha-java-basico/DesafioCollections/src/CarrinhoDeCompras.java
@@ -1,4 +1,85 @@
+import static java.lang.System.out;
+import static java.lang.System.err;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 public class CarrinhoDeCompras {
-    
-    private record Item( String nome, double preco, int quantidade ) {}
+
+    private List<Item> carrinho;
+
+    CarrinhoDeCompras() {
+        carrinho = new ArrayList<>();
+    }
+
+    public static void main( String... args ) {
+        var compras = new CarrinhoDeCompras();
+
+        try{
+            compras.adicionarItem( "Arroz 5kg-T1 ", 35.30, 2 );
+            compras.adicionarItem( "Feijao Branco 1kg ", 7.29, 2 );
+            compras.adicionarItem( "Patinho moído 1kg", 25.60, 2 );
+            compras.adicionarItem( "Frango sassami 1kg", 15.75, 3 );
+            compras.adicionarItem( "Lagarto redondo 1kg", 35.69, 2 );
+            compras.exibirItens();
+            out.println( "Total: " + compras.calcularValorTotal() + " reais" );
+            // Remove o Patinho moído
+            out.println( 
+                compras.removerItem( "Patinho moído 1kg" ) ? "Item removido com sucesso!" : "Falha ao remover do carrinho!" );
+
+            compras.exibirItens();
+            out.println( "Total: " + compras.calcularValorTotal() + " reais" );
+        }
+        catch( NoSuchElementException e ) {
+            err.printf( "Falha ao calcular total %nErro: %s %n", e.getMessage() );
+        }
+        catch( RuntimeException e ) {
+            err.printf( "Falha na adicao ou remocao do item %nErro: %s %n", e.getMessage() );
+        }
+        
+    }
+
+    boolean adicionarItem( String nome, double preco, int quantidade ) throws RuntimeException {
+        return carrinho.add( new Item( nome, preco, quantidade ) );
+    }
+
+    boolean removerItem( String nome ) throws RuntimeException {
+        out.printf( "Buscando pelo item: %s... ", nome );
+        for( var item : carrinho ) 
+            if( item.nome().equalsIgnoreCase( nome ) )
+                return carrinho.remove( item );
+
+        return false;
+    }
+
+    double calcularValorTotal() throws NoSuchElementException {
+        double total = 0;
+        var iterator = carrinho.iterator();
+        
+        while( iterator.hasNext() ) {
+            var item = iterator.next();
+            total += item.preco() * item.quantidade();
+        }
+
+        return total;
+    }
+
+    void exibirItens() {
+
+        out.println( "Itens no carrinho: " );
+        carrinho.stream()
+                .map( 
+                    item -> 
+                        String.format( "item: %-20s valor: %.2f reais \t quantidade: %d", item.nome(), item.preco(), item.quantidade() )
+                )
+                .forEach( out::println );
+    }
+
+    private record Item( String nome, double preco, int quantidade ) implements Comparable<Item> {
+        @Override
+        public int compareTo( Item item ) {
+            return nome().compareTo( nome );
+        }
+    }
 }
