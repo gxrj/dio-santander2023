f82de3b759f3d64587529d2f674f08f5700951c4
gxrj <gsouzafone@gmail.com>
2023-10-05 04:26:44 -0300

    feat: updates model

--- desafio-final-backend-dio-santander2023/src/main/java/me/dio/gxrj/desafiofinalbackenddiosantander2023/domain/model/Funcionamento.java
+++ desafio-final-backend-dio-santander2023/src/main/java/me/dio/gxrj/desafiofinalbackenddiosantander2023/domain/model/Funcionamento.java
@@ -2,14 +2,11 @@ package me.dio.gxrj.desafiofinalbackenddiosantander2023.domain.model;
 
 
 import java.time.LocalTime;
-import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,9 +15,6 @@ import lombok.Setter;
 @Getter @Setter
 public class Funcionamento {
 
-    @Id
-    @GeneratedValue
-    private UUID id;
     @Enumerated( EnumType.STRING )
     private DiaSemana dia;
     @Column( name = "abetura", nullable = false, columnDefinition = "TIME" )
--- desafio-final-backend-dio-santander2023/src/main/java/me/dio/gxrj/desafiofinalbackenddiosantander2023/domain/model/Telefone.java
+++ desafio-final-backend-dio-santander2023/src/main/java/me/dio/gxrj/desafiofinalbackenddiosantander2023/domain/model/Telefone.java
@@ -3,10 +3,6 @@ package me.dio.gxrj.desafiofinalbackenddiosantander2023.domain.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,14 +10,9 @@ import lombok.Setter;
 @Embeddable
 @Getter @Setter
 public class Telefone {
-    @Id
-    @GeneratedValue
-    private Long id;    
+   
     @Column( nullable = false )
     private Integer ddd;
     @Column( nullable = false )
     private String numero;
-    @ManyToOne
-    @JoinColumn( name = "client_id", referencedColumnName = "id" )
-    private Cliente cliente;
 }
