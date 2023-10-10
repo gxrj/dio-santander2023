0dd14bd20229630cbeaeb5c70f1c070b3a580653
gxrj <gsouzafone@gmail.com>
2023-10-09 22:58:18 -0300

    feat: atualiza arquivos

--- desafio-final-backend-dio-santander2023/src/test/java/me/dio/gxrj/desafiofinalbackenddiosantander2023/AppTest.java
+++ desafio-final-backend-dio-santander2023/src/test/java/me/dio/gxrj/desafiofinalbackenddiosantander2023/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DesafioFinalBackendDioSantander2023ApplicationTests {
+class AppTest {
 
 	@Test
 	void contextLoads() {
